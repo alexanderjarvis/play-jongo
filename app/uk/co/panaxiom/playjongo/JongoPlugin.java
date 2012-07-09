76aac917371f733a1ebc0a94a8884388a0d2fcd9
Alex Jarvis <alex@panaxiom.co.uk>
2012-07-09 20:42:25 +0100

    Removed close because it was not necessary and it caused functional tests to fail

--- app/uk/co/panaxiom/playjongo/JongoPlugin.java
+++ app/uk/co/panaxiom/playjongo/JongoPlugin.java
@@ -13,10 +13,5 @@ public class JongoPlugin extends Plugin {
 	public void onStart() {
 		PlayJongo.getInstance();
 	}
-   
-	@Override
-	public void onStop() {
-		PlayJongo.mongo().close();
-	}
 
 }

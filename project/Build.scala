247876988bbf013055189d66f5671d9f058b182d
Alex Jarvis <alex@panaxiom.co.uk>
2012-08-08 09:16:19 +0100

    Updated app version to 0.3-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -5,11 +5,11 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
     val appName         = "play-jongo"
-    val appVersion      = "0.2"
+    val appVersion      = "0.3-SNAPSHOT"
 
     val appDependencies = Seq(
       "org.mongodb" % "mongo-java-driver" % "2.8.0", 
-      "org.jongo" % "jongo" % "0.2"
+      "org.jongo" % "jongo" % "0.3-SNAPSHOT"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(

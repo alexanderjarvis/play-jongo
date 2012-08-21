46cbe38af99ead2af4651be033508a97e3f97a04
Steve Newson <Steven.Newson@gmail.com>
2012-08-21 13:45:11 +0100

    Switched indentation to spaces
    Converted the indentation on the source files to be two spaces rather
    than tab characters. Prevents annoying alignment issues when someone
    (like me) uses spaces by default rather than tabs.

--- app/uk/co/panaxiom/playjongo/PlayJongo.java
+++ app/uk/co/panaxiom/playjongo/PlayJongo.java
@@ -15,69 +15,68 @@ import com.mongodb.gridfs.GridFS;
 
 public class PlayJongo {
 
-	private static volatile PlayJongo INSTANCE = null;
+  private static volatile PlayJongo INSTANCE = null;
 
-	private Mongo mongo = null;
-	private Jongo jongo = null;
-	private GridFS gridfs = null;
-	
-	private PlayJongo() throws UnknownHostException, MongoException {
-		String host = Play.application().configuration().getString("playjongo.host");
-		int port = Play.application().configuration().getInt("playjongo.port");
-		String db = Play.application().configuration().getString("playjongo.db");
-		if (Play.isTest()) {
-			db = "test";
-		}
-		
-		boolean gridfsEnabled = Play.application().configuration().getBoolean("playjongo.gridfs.enabled");
-		
-		mongo = new Mongo(host, port);
-		jongo = new Jongo(mongo.getDB(db));
-		if (gridfsEnabled) {
-			gridfs = new GridFS(jongo.getDatabase());
-		}
-	}
-	
-	public static PlayJongo getInstance() {
-		if (INSTANCE == null) {
-			synchronized (PlayJongo.class) {
-				if (INSTANCE == null) {
-					try {
-						INSTANCE = new PlayJongo();
-					} catch (UnknownHostException e) {
-						Logger.error("UnknownHostException", e);
-					} catch (MongoException e) {
-						Logger.error("MongoException", e);
-					}
-				}
-			}
-		}
-		return INSTANCE;
-	}
-	
-	public static void forceNewInstance() {
-		INSTANCE = null;
-		getInstance();
-	}
+  private Mongo mongo = null;
+  private Jongo jongo = null;
+  private GridFS gridfs = null;
 
-	public static Mongo mongo() {
-		return getInstance().mongo;
-	}
+  private PlayJongo() throws UnknownHostException, MongoException {
+    String host = Play.application().configuration().getString("playjongo.host");
+    int port = Play.application().configuration().getInt("playjongo.port");
+    String db = Play.application().configuration().getString("playjongo.db");
+    if (Play.isTest()) {
+      db = "test";
+    }
 
-	public static Jongo jongo() {
-		return getInstance().jongo;
-	}
-	
-	public static GridFS gridfs() {
-		return getInstance().gridfs;
-	}
-	
-	public static MongoCollection getCollection(String name) {
-		return getInstance().jongo.getCollection(name);
-	}
-	
-	public static DB getDatabase() {
-		return getInstance().jongo.getDatabase();
-	}
+    boolean gridfsEnabled = Play.application().configuration().getBoolean("playjongo.gridfs.enabled");
 
+    mongo = new Mongo(host, port);
+    jongo = new Jongo(mongo.getDB(db));
+    if (gridfsEnabled) {
+      gridfs = new GridFS(jongo.getDatabase());
+    }
+  }
+
+  public static PlayJongo getInstance() {
+    if (INSTANCE == null) {
+      synchronized (PlayJongo.class) {
+        if (INSTANCE == null) {
+          try {
+            INSTANCE = new PlayJongo();
+          } catch (UnknownHostException e) {
+            Logger.error("UnknownHostException", e);
+          } catch (MongoException e) {
+            Logger.error("MongoException", e);
+          }
+        }
+      }
+    }
+    return INSTANCE;
+  }
+
+  public static void forceNewInstance() {
+    INSTANCE = null;
+    getInstance();
+  }
+
+  public static Mongo mongo() {
+    return getInstance().mongo;
+  }
+
+  public static Jongo jongo() {
+    return getInstance().jongo;
+  }
+
+  public static GridFS gridfs() {
+    return getInstance().gridfs;
+  }
+
+  public static MongoCollection getCollection(String name) {
+    return getInstance().jongo.getCollection(name);
+  }
+
+  public static DB getDatabase() {
+    return getInstance().jongo.getDatabase();
+  }
 }

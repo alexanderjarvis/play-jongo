e4c7304dec54682fcac3fc3499b252d8cc9d1d18
Alex Jarvis <alex@panaxiom.co.uk>
2012-10-01 21:40:34 +0100

    Updated jongo early version and play version

--- project/Build.scala
+++ project/Build.scala
@@ -8,8 +8,8 @@ object ApplicationBuild extends Build {
     val appVersion      = "0.3-early"
 
     val appDependencies = Seq(
-      "org.mongodb" % "mongo-java-driver" % "2.8.0", 
-      "org.jongo" % "jongo" % "0.3-early-20120905-1054"
+      "org.mongodb" % "mongo-java-driver" % "2.8.0",
+      "org.jongo" % "jongo" % "0.3-early-20120924-1510"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(

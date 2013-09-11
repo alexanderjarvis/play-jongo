9ea5e6e3aa9e68d9e4ab26b71e22b0b79a3dbfbe
Martin Grotzke <martin.grotzke@googlemail.com>
2013-09-11 22:16:15 +0200

    Pushed version to 0.5.0-jongo0.4, added maven/sonatype publishing info

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ import de.johoop.findbugs4sbt.FindBugs._
 object ApplicationBuild extends Build {
 
     val appName         = "play-jongo"
-    val appVersion      = "0.4"
+    val appVersion      = "0.5.0-jongo0.4"
 
     val appDependencies = Seq(
       "org.mongodb" % "mongo-java-driver" % "2.11.1",
@@ -17,10 +17,46 @@ object ApplicationBuild extends Build {
     val main = play.Project(appName, appVersion, appDependencies,
       settings = Defaults.defaultSettings ++ findbugsSettings).settings(
       organization := "uk.co.panaxiom",
-      javacOptions ++= Seq("-source", "1.6", "-target", "1.6", "-encoding", "UTF-8")
-      //resolvers += ("cloudbees-jongo-early-release" at "http://repository-jongo.forge.cloudbees.com/release")
+      description := "Play 2.1 Module for Jongo http://jongo.org/",
+      javacOptions ++= Seq("-source", "1.6", "-target", "1.6", "-encoding", "UTF-8"),
+      // Maven publishing info
+      publishMavenStyle := true,
+      publishTo := {
+        val nexus = "https://oss.sonatype.org/"
+        if (appVersion.trim.endsWith("SNAPSHOT"))
+          Some("snapshots" at nexus + "content/repositories/snapshots")
+        else
+          Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+      },
+      publishArtifact in Test := false,
+      pomIncludeRepository := { _ => false },
+      pomExtra := extraPom
     )
 
-
+    def extraPom = (
+      <url>https://github.com/alexanderjarvis/play-jongo</url>
+      <licenses>
+        <license>
+          <name>Apache 2.0</name>
+          <url>http://www.apache.org/licenses/LICENSE-2.0</url>
+          <distribution>repo</distribution>
+        </license>
+      </licenses>
+      <scm>
+        <url>git@github.com:alexanderjarvis/play-jongo.git</url>
+        <connection>scm:git:git@github.com:alexanderjarvis/play-jongo.git</connection>
+      </scm>
+      <developers>
+        <developer>
+          <id>alexjarvis</id>
+          <name>Alexander Jarvis</name>
+          <url>https://github.com/alexanderjarvis</url>
+        </developer>
+        <developer>
+          <id>martin.grotzke</id>
+          <name>Martin Grotzke</name>
+          <url>https://github.com/magro</url>
+        </developer>
+      </developers>)
 
 }
